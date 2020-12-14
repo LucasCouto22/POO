9b126886abbb70e65bfd6aeaa0f4107a2e7b9d93
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2020-12-13 21:05:44 -0300

    Update Circulo.java

--- Semana5/1/src/Circulo.java
+++ Semana5/1/src/Circulo.java
@@ -1,12 +1,14 @@
 public class Circulo extends FormaGeometrica {
 
-   public void calcularArea(float raio) {
-	    this.area = raio * raio * 3;
+   public void calculandoArea(float raio) {
+	    
+	   this.area = raio * raio * 3;
 	
     }
 
     public void calcularPerimetro(float raio) {
-        this.perimetro = 6 *raio;
+        	
+	    this.perimetro = 6 * raio;
 
     }
 }
