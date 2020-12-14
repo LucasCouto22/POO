e2457ec31345bb7c81956eb68b7a78ee3d241284
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2020-12-14 00:33:54 -0300

    Update ContaCorrente.java

--- Semana 6 - 18112020/Questão 2/src/ContaCorrente.java
+++ Semana 6 - 18112020/Questão 2/src/ContaCorrente.java
@@ -1,31 +1,47 @@
 public class ContaCorrente {
+    
+    
     float limite = 0;
     float saldo = 0;
-    float valorLimite = 0;
+    float valorDeLimite = 0;
     
 
 
     public void sacar(float valor){
-        if(valor <=0){
-            throw new IllegalArgumentException("Impossivel sacar um valor negativo ou nulo");
+        
+        if(valor <= 0){
+            
+            throw new IllegalArgumentException("Você não pode sacar um valor negativo!");
+            
         }
         if(valor > this.saldo){
-            throw new IllegalArgumentException("Impossivel sacar um valor maior que o saldo");
+            
+            throw new IllegalArgumentException("Você não pode sacar um valor maior que o seu saldo!");
+            
         }
+        
         this.saldo = this.saldo - valor;
+        
     }
 
     public void depositar(float valor){
+        
         if(valor <=0){
-            throw new IllegalArgumentException("Impossivel depositar um valor negativo ou nulo");
+            
+            throw new IllegalArgumentException("Você não pode depositar um valor nulo ou negativo!");
         }
+        
         this.saldo = this.saldo + valor;
     }
 
-    public void setValorLimite(float valorLimite){
-        if(valorLimite < 0){
-            throw new IllegalArgumentException("Impossivel o valor limite ser negativo");
+    public void setValorLimite(float valorDeLimite){
+        
+        if(valorDeLimite < 0){
+            
+            throw new IllegalArgumentException("O valor do limite não pode ser negativo!");
+            
         }
-        this.valorLimite = valorLimite;
+        
+        this.valorDeLimite = valorDeLimite;
     }
 }
