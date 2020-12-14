e05b4a1d6e20a54b21f165c50616062c3579d1fa
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2020-12-14 00:42:23 -0300

    Update CadastroCliente.java

--- Semana 6 - 18112020/Questão 3/src/CadastroCliente.java
+++ Semana 6 - 18112020/Questão 3/src/CadastroCliente.java
@@ -1,26 +1,45 @@
 public class CadastroCliente {
+    
     Cliente[] cadastro = new Cliente[10];
     int clientes = 0;
 
     public void inserir(Cliente cliente) {
+        
         if (this.clientes == 10) {
-            throw new RepositorioExeptions("Numero maximo de clientes atigindo");
+            
+            throw new RepositorioExeptions("Já atingimos o número máximo de clientes!");
+            
         } else {
+            
             if (buscar(cliente.getCPF())) {
-                throw new ClienteJaExistenteException("O cliente ja existe no cadastro");
+                
+                throw new ClienteJaExistenteException("Cliente com cadastro já existente!");
+                
             } else {
+                
                 this.cadastro[clientes] = cliente;
                 this.clientes = this.clientes + +1;
+                
             }
+            
         }
+        
     }
 
     public boolean buscar(String cpf) {
+        
         for (Cliente aux : this.cadastro) {
+            
             if (cpf == aux.getCPF()) {
+                
                 return true;
+                
             }
+            
         }
-        throw new ClienteInexistenteException("Cliente não cadastrado");
+        
+        throw new ClienteInexistenteException("O Cliente não foi cadastrado!");
+        
     }
+    
 }
