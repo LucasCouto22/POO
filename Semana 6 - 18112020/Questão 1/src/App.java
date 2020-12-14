6efd06b0e5a08249984b75f0956b743a40127ac3
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2020-12-14 00:39:49 -0300

    Update App.java

--- Semana 6 - 18112020/Questão 1/src/App.java
+++ Semana 6 - 18112020/Questão 1/src/App.java
@@ -1,36 +1,54 @@
-// A exceção IllegalArgumentException é levantada quando, dentro de um método, for passado um argumento ilegal ou com formato inapropriado
-// A exceção do tipo NumberFormatException é levantada quando, numa tentativa de conversão de string para um numero, apresentar que a string não possuir o formato correto
+// IllegalArgumentException só acontece dentro de um método, se for passada por um argumento ilegal ou formato inapropriado
+// NumberFormatException só acontece quando ocorre a tentativa de conversão de string para número, pois a string não possui o formato correto 
+
 public class App {
-    public static String concatenar(String a, String b){
-        if(a == null){
-            throw new IllegalArgumentException("A primeira string não pode ser nula");
+    public static String concatenar(String primeira, String segunda){
+        if(primeira == null){
+            
+            throw new IllegalArgumentException("A string da primeira variavel não pode ser nula!");
+            
         }
-        if(b == null){
-            throw new IllegalArgumentException("A segunda string não pode ser nula");
+        if(segunda == null){
+            
+            throw new IllegalArgumentException("A segunda variável String não pode ser nula!");
         }
-        return a + b;
+        
+        return primeira + segunda;
 
     }
 
 
     public static void main(String[] args){
-        String a = "123";
-        String b = "Teste";
-        Integer c;
+        
+        String primeira = "123";
+        String segunda = "Teste";
+        Integer tereceira;
+        
         try{
-            App.concatenar(a, null);
+            
+            App.concatenar(primeira, null);
+            
         }
         catch(IllegalArgumentException err){
-            System.out.println("Um argumento ilegal foi passado");
+            
+            System.out.println("Foi passado um argumento ilegal!");
             System.out.println(err.getMessage());
+            
         }
         try{
-            c = Integer.parseInt(b);
+            
+            terceira = Integer.parseInt(segunda);
+            
         }
         catch(NumberFormatException err){
-            System.out.println("Uma conversão ilegal de String para Inteiro foi feita");
+            
+            System.out.println("Não pode ser feita a conversão, uma conversão ilegal foi feita!");
             System.out.println(err.getMessage());
+            
         }
-        System.out.println("Um exemplo de codigo importante");
+        
+        System.out.println("Exemplo!");
+        
     }
+    
 }
