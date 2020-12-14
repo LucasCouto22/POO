c6725c22ab300db880f2311b5b139c9a31cf7945
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2020-12-14 03:55:14 -0300

    Update Counter.java

--- Semana9/2/src/Counter.java
+++ Semana9/2/src/Counter.java
@@ -2,20 +2,33 @@ public class Counter{
     private int count = 0;
 
     synchronized void increment(){
+        
         if(count < 3){
+            
             this.count++;
-            System.out.println("Contador incrementado, novo valor : " + Integer.toString(this.count));
+            System.out.println("O contador recebeu um incremento, agora o valor é de : " + Integer.toString(this.count));
+            
         }else{
-            System.out.println("Contador no limite, esperando ele diminuir");
+            
+            System.out.println("O contador chegou ao seu limite de 3, aguarde que ele diminua!");
+            
         }
 
     }
+    
     synchronized void decrement(){
+        
         if(count > 0){
+            
             this.count--;
-            System.out.println("Contador decrementado, novo valor : " + Integer.toString(this.count));
+            System.out.println("O contador teve um decremento, agora o valor é de : " + Integer.toString(this.count));
+            
         }else{
-            System.out.println("Contador no limite, esperando ele aumentar");
+            
+            System.out.println("O contador chegou ao seu limite de 0, aguarde que ele aumente!");
+            
         }
+        
     }
+    
 }
