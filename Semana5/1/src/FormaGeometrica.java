afd0413c564d2246d588945673ee875ac3f5d5c6
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2020-12-13 21:06:20 -0300

    Update FormaGeometrica.java

--- Semana5/1/src/FormaGeometrica.java
+++ Semana5/1/src/FormaGeometrica.java
@@ -1,12 +1,17 @@
 public abstract class FormaGeometrica {
+    
     public float area;
     public float perimetro;
 
     public float getArea(){
+        
         return this.area;
+    
     }
 
     public float getPerimetro(){
+        
         return this.perimetro;
+        
     }
 }
