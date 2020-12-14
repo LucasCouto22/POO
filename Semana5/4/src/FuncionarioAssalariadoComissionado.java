d571d2d0c8dc15ba4aee002aaeea25df4446f5ca
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2020-12-13 21:12:56 -0300

    Update FuncionarioAssalariadoComissionado.java

--- Semana5/4/src/FuncionarioAssalariadoComissionado.java
+++ Semana5/4/src/FuncionarioAssalariadoComissionado.java
@@ -1,9 +1,12 @@
 public class FuncionarioAssalariadoComissionado extends Funcionario{
-    private float SalarioBase = 900;
+    
+    private float salarioBase = 900;
     private float valorVendas = 5000;
 
     public double pagarFuncionario(){
-        return (this.SalarioBase + 0.1 * this.valorVendas) * 1.1;
+        
+        return (this.salarioBase + 0.1 * this.valorVendas) * 1.1;
+        
     }
     
 }
