4a2bfb52c6cfa9bbf49074cb0691c1f9fec31111
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2020-12-14 01:07:22 -0300

    Update ClienteInexistenteException.java

--- Semana7/1/src/ClienteInexistenteException.java
+++ Semana7/1/src/ClienteInexistenteException.java
@@ -1,10 +1,8 @@
 public class ClienteInexistenteException extends RuntimeException {
-    /**
-     *
-     */
+
     private static final long serialVersionUID = 1L;
 
-    public ClienteInexistenteException(String msg) {
-        super(msg);
+    public ClienteInexistenteException(String mensagem) {
+        super(mensagem);
     }
 }
