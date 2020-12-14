bacf876b2a56286753c3a44fb705b9025ea28fcb
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2020-12-14 03:03:00 -0300

    Update App.java

--- Semana8/3/src/App.java
+++ Semana8/3/src/App.java
@@ -3,10 +3,15 @@ import java.util.LinkedHashSet;
 import java.util.ArrayDeque;
 
 public class App {
+    
     public static void main(String[] args) throws Exception {
-        LinkedHashMap<String, String> musicas = new LinkedHashMap<String, String>();
-        ArrayDeque<String> fila = new ArrayDeque<String>();
-        fila.removeFirst();
-        fila.add();
+        
+        LinkedHashMap<String, String> musica = new LinkedHashMap<String, String>();
+        ArrayDeque<String> filaMusicas = new ArrayDeque<String>();
+        filaMusicas.removeFirst();
+        filaMusicas.add();
+        
     }
+    
+    //ordem: Recebe a música, joga no fim da fila. Lê a fila, toca a música e remove a primeira da fila.
 }
