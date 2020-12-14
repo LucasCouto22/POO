9e7540bf2a007a403bab2d2b59a9efdea16a5a93
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2020-12-14 01:19:31 -0300

    Update criaMenu.java

--- Semana7/1/src/criaMenu.java
+++ Semana7/1/src/criaMenu.java
@@ -3,76 +3,119 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class criaMenu {
+    
     public static void main(String[] args) throws IOException {
+        
         Scanner teclado = new Scanner(System.in);
         int acao;
-        System.out.println("1 - Cadastrar novo cliente");
-        System.out.println("2 - Buscar cliente pelo nome");
-        System.out.println("3 - Buscar cliente pelo CPF");
-        System.out.println("4 - Excluir cliente");
-        System.out.println("5 - Listar cliente");
+        System.out.println("1 - Para cadastrar novo cliente.");
+        System.out.println("2 - Para buscar um cliente pelo nome.");
+        System.out.println("3 - Para buscar um cliente pelo CPF.");
+        System.out.println("4 - Para excluir um cliente.");
+        System.out.println("5 - Para listar clientes.");
         acao = teclado.nextInt();
+        
         if(acao == 1){
+            
             String cpf;
             String nome;
             String endereco;
             String dataNascimento;
-            System.out.println("Digite o CPF do cliente");
+            System.out.println("Informe o CPF do cliente: ");
             cpf = teclado.nextLine();
-            System.out.println("Digite o nome");
+            
+            System.out.println("Informe o nome do cliente: ");
             nome = teclado.nextLine();
-            System.out.println("Digite o endereço");
+            
+            System.out.println("Informe o endereço do cliente: ");
             endereco = teclado.nextLine();
-            System.out.println("Digite a data de nascimento");
+            
+            System.out.println("Informe a data de nascimento do cliente: ");
             dataNascimento = teclado.nextLine();
+            
             try{
+                
             CadastroCliente.inserir(new Cliente(cpf, nome, endereco, dataNascimento));
+                
             }
+            
             catch (ClienteInexistenteException err){
-                System.out.println("Cliente cadastrado com sucesso");
+                
+                System.out.println("O cliente foi cadastrado com sucesso!");
+                
             }
+            
         }
+        
         else if(acao == 2){
-            System.out.println("Digite o nome do cliente");
+            
+            System.out.println("Informe o nome do cliente: ");
             String nome = teclado.nextLine();
+            
             ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+            
             try{
+                
                 clientes = CadastroCliente.buscar_nome(nome);
                 for(Cliente cliente : clientes)
                 System.out.println(cliente);
+                
             } catch (ClienteInexistenteException err){
+                
                 System.out.println(err);
+                
             }
+            
         }
         else if(acao ==3){
+            
             String cpf;
-            System.out.println("Digite o CPF do cliente");
+            System.out.println("Informe o CPF do cliente: ");
             cpf = teclado.nextLine();
             Cliente cliente = new Cliente();
+            
             try{
+                
                 cliente = CadastroCliente.buscar_cpf(cpf);
                 System.out.println(cliente);
+                
             }catch (ClienteInexistenteException err){
+                
                 System.out.println(err);
+                
             }
+            
         }
         else if(acao == 4){
+            
             String cpf;
-            System.out.println("Digite o CPF do cliente");
+            System.out.println("Informe o CPF do cliente: ");
             cpf = teclado.nextLine();
             boolean corretamente = CadastroCliente.excluir(cpf);
+            
             if(corretamente){
-                System.out.println("O cliente foi excluido com sucesso!");
+                
+                System.out.println("Cliente excluido!");
+                
             }else{
-                System.out.println("Não foi possivel excluir o cliente");
+                
+                System.out.println("Não foi possivel executar a exclusão do cliente!");
+                
             }
+            
         }
         else if(acao == 4){
+            
             try{
+                
                 CadastroCliente.listar();
+                
             } catch (ClienteInexistenteException err){
+                
                 System.out.println(err);
+                
             }
+            
         }
         teclado.close();
     }
