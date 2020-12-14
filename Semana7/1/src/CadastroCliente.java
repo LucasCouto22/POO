70f0c885a74701d9f168658754a98e0bf45f56ca
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2020-12-14 00:49:32 -0300

    Update CadastroCliente.java

--- Semana7/1/src/CadastroCliente.java
+++ Semana7/1/src/CadastroCliente.java
@@ -2,38 +2,62 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class CadastroCliente {
+    
     ArrayList<Cliente> cadastro = new ArrayList<Cliente>();
 
     static void inserir(Cliente cliente) throws IOException {
+        
         try {
+            
             buscar_cpf(cliente.getCPF());
-            throw new ClienteJaExistenteException("“Não foi possível cadastrar o cliente");
+            throw new ClienteJaExistenteException("Cadastão de cliente não foi possível!");
+            
         } catch (ClienteInexistenteException e) {
+            
             ClienteArquivo.inserir(cliente);
+            
         }
+        
     }
 
     static Cliente buscar_cpf(String cpf) throws IOException {
+        
         return ClienteArquivo.buscarPorCPF(cpf);
+        
     }
     static ArrayList<Cliente> buscar_nome(String nome) throws IOException {
+        
         return ClienteArquivo.bucasPorNome(nome);
+        
     }
 
     static void listar() throws IOException {
+        
         ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+        
         clientes = ClienteArquivo.listar();
+        
         for(Cliente cliente: clientes){
+            
             System.out.println(cliente);
+            
         }
+        
     }
 
     static boolean excluir(String cpf) throws IOException {
+        
         try{
+            
             ClienteArquivo.excluir(cpf);
             return true;
+            
         }catch(IOException err){
+            
             return false;
+            
         }
+        
     }
+    
 }
