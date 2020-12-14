7a9b3387b3ef98ffd454eb9ac51304726cdf7f2a
LucasCouto22 <62523407+LucasCouto22@users.noreply.github.com>
2020-12-14 03:00:03 -0300

    Update App.java

--- Semana8/2/src/App.java
+++ Semana8/2/src/App.java
@@ -5,27 +5,36 @@ import java.util.PriorityQueue;
 import java.util.TreeSet;
 
 public class App {
+    
     public static void main(String[] args) throws Exception {
+        
         ArrayList<String> arraylist = new ArrayList<String>();
-        arraylist.add("Linha1");
-        arraylist.add("linha2");
-        arraylist.add("linha3");
-        arraylist.add("linha4");
-        arraylist.add("linha5");
-        System.out.println("Array List");
+        arraylist.add("Termo a");
+        arraylist.add("Termo b");
+        arraylist.add("Termo c");
+        arraylist.add("Termo d");
+        arraylist.add("Termo e");
+        System.out.println("Um Array List:");
         System.out.println(arraylist.toString());
+        
         HashSet<String> hashset = new HashSet<String>(arraylist);
-        System.out.println("Hash Set");
+        System.out.println("Um HashSet:");
         System.out.println(hashset.toString());
+        
         PriorityQueue<String> priorityqueue = new PriorityQueue<String>(hashset);
-        System.out.println("Priority Queue");
+        System.out.println("Um PriorityQueue: ");
         System.out.println(priorityqueue.toString());
+        
         ArrayDeque<String> arraydeque = new ArrayDeque<String>(priorityqueue);
-        System.out.println("Array Deque");
+        System.out.println("Um ArrayDeque: ");
         System.out.println(arraydeque.toString());
+        
         TreeSet<String> treeset = new TreeSet<String>(arraydeque);
-        System.out.println("Tree Set");
+        System.out.println("Um TreeSet: ");
         System.out.println(treeset.toString());
-        // As diferenças nas ordens se da pelas diferentes maneiras de armazenar os dados. Cada classe se utiliza de uma tecnica de estrutura de dados diferentes
+        
     }
+    
 }
+
+// As diferenças estão na forma em que se armazenam os dados. Como vimos na disciplina Estrutura de Dados, existem diversas formas de se armazenar um dado, cada uma tem o seu propósito onde se torna melhor. 
